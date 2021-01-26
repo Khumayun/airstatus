b411092b9df6c25c6bc8f319ec457f33e6d7138c
Khumayun <huba4buba@gmail.com>
2021-01-26 23:07:00 +0500

    move staticfiles to /var/www/static/

--- airstatus/settings.py
+++ airstatus/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/root/airstatus/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/root/airstatus'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = list(default_methods) + [
