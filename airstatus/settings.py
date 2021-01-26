234604c1bfd29bd952ddd89349a19c83d40b1b7b
Khumayun <huba4buba@gmail.com>
2021-01-26 23:10:29 +0500

    db fix

--- airstatus/settings.py
+++ airstatus/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'airstatus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'airstatus',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
