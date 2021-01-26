4faab2abbb462511d16516f9e75b6630dcae0384
Khumayun <huba4buba@gmail.com>
2021-01-26 19:26:07 +0500

    minor changes

--- airstatus/settings.py
+++ airstatus/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = list(default_methods) + [
