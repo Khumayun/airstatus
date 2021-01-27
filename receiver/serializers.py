531e005ffa360b7fc9d0863f1846d01a4afbe9bc
Khumayun <huba4buba@gmail.com>
2021-01-27 16:20:24 +0500

    new feauture

--- receiver/serializers.py
+++ receiver/serializers.py
@@ -6,6 +6,21 @@ class PMSerializer(serializers.ModelSerializer):
 		model = PM
 		fields = '__all__'
 
+class PM1Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = PM
+		fields = ['pm1']
+
+class PM25Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = PM
+		fields = ['pm25']
+
+class PM10Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = PM
+		fields = ['pm10']
+
 class TemperatureSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Temperature
